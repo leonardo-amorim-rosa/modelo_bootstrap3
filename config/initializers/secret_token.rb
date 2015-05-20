ac21ea23537c4edbb72eed6853b7ca16ee1f567a
leonardo <leoamorimrosa@gmail.com>
2015-05-20 10:41:45 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModeloBootstrap3::Application.config.secret_token = '7413a1b7881514b2fc52f5690d90c2310c8084280adbc4def3322e699d6d08b4edf3500e6e9ac2c64a96b9d87cf5655034987ccd2420a03ea19470d769003a4c'
